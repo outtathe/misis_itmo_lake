c76a00f5a545e09127db8c591b027a1a2ea15231
wilhemiv <wilhemiv@wstudent.21-school.ru>
2024-10-17 21:06:24 +0300

    c

--- app/graph.py
+++ app/graph.py
@@ -1,12 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Данные для графика
-queries = np.arange(1, 11)  # Количество запросов от 1 до 10
-before_z_ordering = [50, 48, 47, 46, 45, 44, 43, 42, 41, 40]  # Время выполнения до оптимизации (в секундах)
-after_z_ordering = [30, 28, 27, 26, 25, 24, 23, 22, 21, 20]  # Время выполнения после Z-Ordering (в секундах)
+queries = np.arange(1, 11)
+before_z_ordering = [50, 48, 47, 46, 45, 44, 43, 42, 41, 40]  
+after_z_ordering = [30, 28, 27, 26, 25, 24, 23, 22, 21, 20] 
 
-# Построение графика
 plt.figure(figsize=(10, 6))
 plt.plot(queries, before_z_ordering, label='До Z-Ordering', marker='o')
 plt.plot(queries, after_z_ordering, label='После Z-Ordering', marker='o')
